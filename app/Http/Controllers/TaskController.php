2231a688212d2abb87d47b6cd1ab737115d7cb26
Kenneth <anthonyedem46@gmail.com>
2024-05-23 14:55:58 +0000

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $task = Task::create($request->all());
+
+        return response()->json($task, 201);
+    }
+
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'sometimes|required|string|max:255',
+        ]);
+
+        $task->update($request->all());
+
+        return response()->json($task, 200);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json(null, 204);
+    }
+}
